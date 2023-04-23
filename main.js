758a0c4de02abd184950d65d84adf9efaee139cb
sometimesbread <82068136+sometimesbread@users.noreply.github.com>
2023-04-23 12:44:08 -0700

    pixel renders now but really slowly

--- main.js
+++ main.js
@@ -10,11 +10,32 @@ function RenderImage()
             return response.json();
         })
         .then((returnjson) => {
-            for(let i = 0; i < returnjson.pixelCount; i++)
+            for(let x = 0; x < returnjson.w; x++)
             {
-                calculator.setExpression({id: 'graph1', latex: returnjson.pixels[i], fillOpacity: 1});
-                console.log()
+                for(let y = 0; y < returnjson.h; y++)
+                {
+                    calculator.setExpression({id: x + "x" + y, latex: returnjson.pixels[x * returnjson.h + y], fillOpacity: 1, color: returnjson.colors[x * returnjson.h + y]});
+                    console.log("b")
+                }
             }
         })
     }, 33.33333333);
+}
+
+function once()
+{
+    fetch("http://127.0.0.1/RenderImage")
+        .then((response) => {
+            return response.json();
+        })
+        .then((returnjson) => {
+            for(let x = 0; x < returnjson.w; x++)
+            {
+                for(let y = 0; y < returnjson.h; y++)
+                {
+                    calculator.setExpression({id: x + "x" + y, latex: returnjson.pixels[x * returnjson.h + y], fillOpacity: 1, color: returnjson.colors[x * returnjson.h + y]});
+                    console.log("b")
+                }
+            }
+        })
 }
--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-import mss
+import cv2
+import numpy as np
+from PIL import Image
+from mss import mss
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+
+MONITOR_WIDTH = 2560 #set to your monitor resolution
+MONITOR_HEIGHT = 1440
+
+RESIZE_WIDTH = 32 #set to the resoultion you want in desmos (dont set this too high!!!!)
+RESIZE_HEIGHT = 18
+
+GRAPH_SCALE = 0.5 #resizes the graph because for some reason zooming out too much makes the pixels go woowoooowoowoooo
 
 app = Flask(__name__)
 CORS(app)
@@ -10,10 +21,32 @@ def GetLatex(left, right, top, bottom):
 
 @app.route("/RenderImage")
 def RenderImage():
+    print("a")
+    sct = mss()
     pixels = []
 
-    pixels.append(GetLatex(1,2,4,3))
-    return jsonify({"pixelCount": len(pixels), "pixels": pixels})
+    screenshot = sct.grab({'top': 0, 'left': 0, 'width': MONITOR_WIDTH, 'height': MONITOR_HEIGHT})
+    screenshot_resized = np.array(Image.fromarray(np.array(screenshot)).resize((RESIZE_WIDTH, RESIZE_HEIGHT)))
+
+    screenshotHex = []
+    xb=0
+    for x in screenshot_resized:
+        xb += 1
+        yb = 0
+        for y in x:
+            yb += 1
+            currentHex = "#"
+            for rgb in list(y):
+                if(rgb < 16):
+                    currentHex += "0" + hex(rgb)[2:]
+                else:
+                    currentHex += hex(rgb)[2:]
+                
+            screenshotHex.append(currentHex[:-2])
+            pixels.append(GetLatex(yb, yb+1, xb+1, xb))
+
+    print(screenshotHex)
+    return jsonify({"pixelCount": len(pixels), "pixels": pixels, "colors": screenshotHex, "w": RESIZE_WIDTH, "h": RESIZE_HEIGHT, "scale": GRAPH_SCALE})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
